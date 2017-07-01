6370464603042afbbf02ebdd398926ee1aa9032b
huangzhihu <546835042@qq.com>
2017-07-01 10:24:30 +0800

    the last

---
+++ vue-simple/自定义指令1.vue
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to Vue</title>
+
+  <meta http-equiv="content-type" content="text/html;charset=utf-8">
+  <script src="https://unpkg.com/vue/dist/vue.js"></script>
+  <script src="bower_components/vue-resource/dist/vue-resource.js"></script>
+  <style>
+    .gray {
+      background: #ccc;
+    }
+
+  </style>
+</head>
+<body>
+    <div id="box">
+        <span v-red>
+            asdfasd
+        </span>
+        <span v-red>
+            asdfasd
+        </span>
+        <span v-red>
+            asdfasd
+        </span>
+    </div>
+    <script>
+        Vue.directive('red',function(){
+            this.el.style.background='red';
+        });
+
+        window.onload=function(){
+            var vm=new Vue({
+                el:'#box',
+                data:{
+                    msg:'welcome'
+                }
+            });
+        };
+
+    </script>
+
+</body>
+</html>
+
---
+++ vue-simple/自定义过滤器.vue
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to Vue</title>
+
+  <meta http-equiv="content-type" content="text/html;charset=utf-8">
+  <script src="https://unpkg.com/vue/dist/vue.js"></script>
+  <script src="bower_components/vue-resource/dist/vue-resource.js"></script>
+  <style>
+    .gray {
+      background: #ccc;
+    }
+
+  </style>
+</head>
+<body>
+    <div id="box">
+        {{msg | toDou('12','5')}}
+    </div>
+
+        <script>
+        Vue.filter('toDou',function(n,a,b){
+            alert(a+','+b);
+            //alert(input);
+            return n<10?'0'+n:''+n;
+        });
+
+        window.onload=function(){
+            new Vue({
+                el:'#box',
+                data:{
+                    msg:9
+                }
+            });
+        };
+    </script>
+</body>
+</html>
